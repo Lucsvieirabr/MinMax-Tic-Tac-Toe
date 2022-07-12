52de8908283848e62061747b86862c011b1e38f1
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-07-12 15:07:04 -0300

    Make Winner Line

--- gamefunctions.js
+++ gamefunctions.js
@@ -1,6 +1,8 @@
 let wCell = 420 / 3;
 let hCell = 420 / 3;
 let timeOf = "X";
+let scoreX = 0;
+let scoreO = 0;
 
 function getMouseCord(event){
     let mouseX = event.clientX - canvas.offsetLeft;
@@ -35,22 +37,31 @@ function checkWinner(){
     let winOnCollum =checkColumns();
     let winOnDiagonal = checkDiagonals();
     if(winOnRow || winOnCollum || winOnDiagonal){
+        changeScore(timeOf);
         return true;
     }
 }
 
-function checkRows(){
+function checkColumns(){
     for(let i = 0; i < 3; i++){
         if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ""){
+            let x1 = hCell/2 + i * hCell;
+            let y1 = wCell/2;
+            let y2 = hCell/2 *5;
+            drawnLine(x1, y1, x1, y2, 'red');
             return true;
         }
     }
     return false;
 }
 
-function checkColumns(){
+function checkRows(){
     for(let i = 0; i < 3; i++){
         if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ""){
+            let x1 = wCell/2;
+            let y1 = hCell/2 + i * hCell;
+            let x2 = 2.5 * wCell;
+            drawnLine(x1, y1, x2, y1, 'red');
             return true
         }
     }
@@ -58,8 +69,30 @@ function checkColumns(){
 }
 
 function checkDiagonals(){
-    if(board[1][1] == board[0][0] && board[1][1] == board[2][2] && board[1][1] != "" || board[1][1] == board[0][2] && board[1][1] == board[2][0] && board[1][1] != ""){
+    if(board[1][1] == board[0][0] && board[1][1] == board[2][2] && board[1][1] != ""){
+        let x1 = wCell/2;
+        let y1 = hCell/2;
+        let x2 = wCell *2.5;
+        let y2 = hCell * 2.5;
+        drawnLine(x1, y1, x2, y2, 'red');
+        return true;
+    }
+    if(board[1][1] == board[0][2] && board[1][1] == board[2][0] && board[1][1] != ""){
+        let x1 = wCell * 2.5;
+        let y1 = hCell/2;
+        let x2 = wCell/2;
+        let y2 = hCell * 2.5;
+        drawnLine(x1, y1, x2, y2, 'red');
         return true;
     }
     return false;
+}
+function changeScore(winner){
+    if(winner == "X"){
+        scoreX++;
+        document.getElementById("scoreX").innerHTML = scoreX;
+    }else{
+        scoreO++;
+        document.getElementById("scoreO").innerHTML = scoreO;
+    }
 }
