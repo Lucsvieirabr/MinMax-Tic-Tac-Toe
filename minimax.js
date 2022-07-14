949f1d5eabea08a012c50c2ed2185a59f34a1372
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-07-14 17:50:00 -0300

    ERROR 2

--- minimax.js
+++ minimax.js
@@ -1,32 +1,33 @@
 function makeiamove() {
-    minimax(board,0, []);
+  minimax(board, 0, [], timeOf);
 }
 
-function minimax(IAboard,score, moves) {
-  if(score === 1) {
-    make_move(moves[0][0], moves[0][1], "O");
-    return
-  }
-  if(score === -1) {
-    return
-  }
+function minimax(IAboard, score, moves, player) {
+  
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
       if (IAboard[i][j] == "") {
-        IAboard[i][j] = timeOf;
+        IAboard[i][j] = player;
         moves.push([i, j]);
-        if (checkWin()) {
-          if(timeOf == "X") {
-             score =-1;
-          }else {
+        if (checkWinner()) {
+          if (player == "X") {
+            score = -1;
+          } else {
             score = 1;
           }
-        }else{
-            score = 0;
+        } else {
+          score = 0;
         }
-        minimax(IAboard,score, moves);
+        if (
+          (score = 0 && board.filter((x) => x.filter((y) => y == "").length == 0).length > 0)
+        ) {
+          return;
+        }
+        minimax(IAboard, score, moves, (player == "X") ? "O" : "X");
+        console.log(board);
+        if(i == 3 && j == 3){return}
         IAboard[i][j] = "";
       }
     }
-  }
+  } 
 }
