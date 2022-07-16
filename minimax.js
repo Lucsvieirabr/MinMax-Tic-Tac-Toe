da4a74cda5b9eabf37ec43f9560a674f83c401a8
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-07-16 11:54:03 -0300

    update minimax

--- minimax.js
+++ minimax.js
@@ -27,11 +27,26 @@ function score_all_moves(gameBoard, moves, player) {
   let scoredMoves = [];
   for(let i = 0; i < moves.length; i++){
     gameBoard[moves[i][0]][moves[i][1]] = player;
-    let score = getScore(gameBoard);
-    scoredMoves.push([score]);
-    gameBoard[moves[i][0]][moves[i][1]] = "";
+    if(!hasPossibleMoves(gameBoard)){
+      console.log(gameBoard)
+      let score = getScore(gameBoard);
+      scoredMoves.push([score]);
+      gameBoard[moves[i][0]][moves[i][1]] = "";
+    }else{
+      let newmoves = getPossiblesMoves(gameBoard);
+      score_all_moves(gameBoard, newmoves, (player == "X") ? "O" : "X");
+      
+    }
+    
   }
   return scoredMoves;
+}
+function hasPossibleMoves(gameBoard){
+  let possibleMoves = getPossiblesMoves(gameBoard);
+  if(possibleMoves.length == 0){
+    return false;
+  }
+  return true;
 }
 function getBestMove(moves, scoredMoves) {
   let bestMove = moves[0];
@@ -47,36 +62,7 @@ function getBestMove(moves, scoredMoves) {
 function minimax(gameBoard){
   let possibleMoves = getPossiblesMoves(gameBoard);
   let scoredMoves = score_all_moves(gameBoard, possibleMoves, "O");
+  console.log(scoredMoves);
   let bestMove = getBestMove(possibleMoves, scoredMoves);
   return bestMove;
 }
-//function minimax(IAboard, score, moves, player) {
-  //Fazer uma array com todos os movimentos possíveis e depois percorrer todos os movimentos com o Minimax
-  //Se não tiver nenhum movimento possível, então vai no que tem melhor score
-  //for (i = 0; i < 3; i++) {
-    //for (j = 0; j < 3; j++) {
-      //if (IAboard[i][j] == "") {
-        //console.log('pode jogar em ', i, j);
-        // IAboard[i][j] = player;
-        // moves.push([i, j]);
-        // if (checkWinner()) {
-        //   if (player == "X") {
-        //     score = -1;
-        //   } else {
-        //     score = 1;
-        //   }
-        // } else {
-        //   score = 0;
-        // }
-        // if (
-        //   (score = 0 && board.filter((x) => x.filter((y) => y == "").length == 0).length > 0)
-        // ) {
-        //   return;
-        // }
-        // minimax(IAboard, score, moves, (player == "X") ? "O" : "X");
-        // console.log(board);
-        // IAboard[i][j] = "";
-      //}
-    //}
-  //} 
-//}
