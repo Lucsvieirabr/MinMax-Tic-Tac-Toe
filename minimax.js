80328a31c9fb98426cf91e13a63a145de3e513bc
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-07-19 21:35:34 -0300

    Tst 1

--- minimax.js
+++ minimax.js
@@ -1,7 +1,22 @@
 function makeiamove() {
-  //
+  //pegar todos os movimentos possíveis
+  //pegar todos os possíveis gameboards
+  //Fazer o score de cada gameboard
+  //Pegar o gameboard com o maior score
+  let possiblesmoves = getPossibleMoves(board);
+  let possiblesGameBoards = getPossiblesGameBoards(board, "O");
+  let moveScores = [];
+  for (i = 0; i < possiblesmoves.length; i++) {
+    let gameboard = possiblesGameBoards[i];
+    let score = getMoveScore(gameboard, "O");
+    moveScores.push(score);
+  }
+  let maxScore = Math.max(...moveScores);
+  let maxIndex = moveScores.indexOf(maxScore);
+  let move = possiblesmoves[maxIndex];
+  make_move(move[0], move[1], "O");
 }
-function getPossibleMoves(IAboard){
+function getPossibleMoves(IAboard) {
   let possibleMoves = [];
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
@@ -10,8 +25,9 @@ function getPossibleMoves(IAboard){
       }
     }
   }
+  return possibleMoves;
 }
-function getPossiblesGameBoards(IAboard, player){
+function getPossiblesGameBoards(IAboard, player) {
   let possiblesGameBoards = [];
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
@@ -24,18 +40,53 @@ function getPossiblesGameBoards(IAboard, player){
   }
   return possiblesGameBoards;
 }
-
-function getBoardScore(gBoard){
-  if(checkWinner(gBoard)){
-    if(winner == "X"){
-      return -1;
-    }
-    else{
-      return 1;
+function hasEmptyCells(gBoard) {
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      if (gBoard[i][j] == "") {
+        return true;
+      }
     }
   }
+  return false;
 }
-function minimax(gBoard, player){
+function getMoveScore(IAboard, player) {
+  player = (player == "X") ? "O" : "X";
+  let possibleGameBoards = getPossiblesGameBoards(IAboard, player);
+  let scores = [];
+  for (i = 0; i < possibleGameBoards.length; i++) {
+    let score = getBoardScore(possibleGameBoards[i], player);
+    scores.push(score);
+  }
+  if (
+    scores.filter(function (score) {
+      return score != 0;
+    }).length == 0
+  ) {
+    for (i = 0; i < possibleGameBoards.length; i++) {
+      if (hasEmptyCells(possibleGameBoards[i])) {
+        getMoveScore(possibleGameBoards[i], player == "X" ? "O" : "X");
+      }
+    }
+  }
+ 
+  let maxScore = Math.max(...scores);
+  if (maxScore == 1) {
+    return player == "X" ? -1 : 1;
+  }
+  return -1;
+}
+function getBoardScore(gBoard, player) {
+  if (checkWinner(gBoard)) {
+    if (winner == player) {
+      return 1;
+    }
+    return -1;
+  }
+  return 0;
+}
+function minimax(gBoard, player) {
   let possiblesmoves = getPossibleMoves(gBoard);
   let possiblesGameBoards = getPossiblesGameBoards(gBoard, player);
 }
+
